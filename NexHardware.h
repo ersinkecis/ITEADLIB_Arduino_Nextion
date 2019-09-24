1b7ffcef061289619075ea18f2f3534724e78846
Ersin Kecis <ersinkecis@users.noreply.github.com>
2019-09-24 12:08:06 +0300

    Update NexHardware.h

--- NexHardware.h
+++ NexHardware.h
@@ -18,6 +18,9 @@
 #include "NexConfig.h"
 #include "NexTouch.h"
 
+#include <SoftwareSerial.h>
+extern SoftwareSerial *nexSerial;
+
 /**
  * @addtogroup CoreAPI 
  * @{ 
@@ -29,6 +32,8 @@
  * @return true if success, false for failure. 
  */
 bool nexInit(void);
+bool nexInit(SoftwareSerial *serialPort);
+bool nexInit(int baudDebug, int baudDisplay, int pinRX, int pinTX);
 
 /**
  * Listen touch event and calling callbacks attached before.
@@ -52,4 +57,18 @@ uint16_t recvRetString(char *buffer, uint16_t len, uint32_t timeout = 100);
 void sendCommand(const char* cmd);
 bool recvRetCommandFinished(uint32_t timeout = 100);
 
+/*
+ * Code added: 
+ * Detect if the device is in sleepMode
+ * sleepModeNextion returns if the device automatically enters sleep mode
+ * setSleepNexsetSleepNextion allows you to modify the variable that stores whether it is in sleep mode or not, 
+ * useful when the device wakes up by command and automatically. It allows you to modify the variable that stores 
+ * whether it is in sleep mode or not, useful when the device wakes up by command and automatically.
+ */
+bool sleepModeNextion();
+bool setSleepNextion(bool status);
+/*
+ * End code added.
+ */
+ 
 #endif /* #ifndef __NEXHARDWARE_H__ */
